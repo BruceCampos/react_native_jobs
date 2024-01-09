5d4209cd2ddd72567a907180ee85a5b89795020c
Bruce Campos <brucecamposnh@gmail.com>
2024-01-09 13:05:16 -0300

    home

---
+++ hook/useFetch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const useFetch = () => {
+  return (
+    <View>
+      <Text>useFetch</Text>
+    </View>
+  )
+}
+
+export default useFetch
